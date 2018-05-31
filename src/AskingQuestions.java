6ad9bbb3f4c191b40ce32762e820668d090b7101
nosukja99 <nosukja99@gmail.com>
2018-05-31 15:14:44 -0400

    Initial commit

---
+++ src/AskingQuestions.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class AskingQuestions
+{
+    public static void main( String[] args )
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        double num1, num2, num3;
+
+        System.out.print( "First temperature? " );
+        num1 = keyboard.nextInt();
+
+        System.out.print( "Second temperature? " );
+        num2 = keyboard.nextInt();
+
+        System.out.println("The max value is : " + Math.max(num1, num2));
+
+        num3 = (num1+num2)/2;
+        System.out.println("The average of "+num1+" and "+num2+" is "+ num3);
+    }
+}
