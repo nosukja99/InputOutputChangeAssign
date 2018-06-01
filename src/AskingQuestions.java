698729637b428083897779adb6b041b60565883f
nosukja99 <nosukja99@gmail.com>
2018-06-01 10:56:44 -0400

    corrected

--- src/AskingQuestions.java
+++ src/AskingQuestions.java
@@ -6,13 +6,14 @@ public class AskingQuestions
     {
         Scanner keyboard = new Scanner(System.in);
 
-        double num1, num2, num3;
+        double num1, num2;
+        double num3;
 
         System.out.print( "First temperature? " );
-        num1 = keyboard.nextInt();
+        num1 = keyboard.nextDouble();
 
         System.out.print( "Second temperature? " );
-        num2 = keyboard.nextInt();
+        num2 = keyboard.nextDouble();
 
         System.out.println("The max value is : " + Math.max(num1, num2));
 
